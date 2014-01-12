6ec2a05667d1359cbb6d32a61532c6704d11a31b
Mark Kenneth Ranosa <mark.ranosa@gmail.com>
2014-01-13 01:52:33 +0800

    Added email functionality

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,23 +1,39 @@
 'use strict';
 
 angular.module('nciFinalFrontApp')
-	.controller('ContactCtrl', function($scope, $http) {
+	.controller('ContactCtrl', function($scope, $http, NotificationService) {
 
-		$http.post('https://mandrillapp.com/api/1.0/messages/send.json', {
-			'key': 'ntAQV7lrBD2pOtGwIUHw_A',
-			'message': {
-				'from_email': 'nci.final@gmail.com',
-				'to': [{
-					'email': 'mark.ranosa@gmail.com',
-					'name': 'RECIPIENT NAME (OPTIONAL)',
-					'type': 'to'
-				}],
-				'autotext': 'true',
-				'subject': 'YOUR SUBJECT HERE!',
-				'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
-			}
-		}).success(function(response) {
-			console.log(response);
-		});
+
+
+		$scope.email = {
+			name: '',
+			from: '',
+			message: ''
+		};
+
+		$scope.sendEmail = function() {
+			NotificationService.info('Sending Email!', 'Sending email in progress');
+			$http.post('https://mandrillapp.com/api/1.0/messages/send.json', {
+				'key': 'ntAQV7lrBD2pOtGwIUHw_A',
+				'message': {
+					'from_email': $scope.email.from,
+					'to': [{
+						'email': 'nci.final@gmail.com',
+						'name': $scope.email.name,
+						'type': 'to'
+					}],
+					'autotext': 'true',
+					'subject': 'Message from ' + $scope.email.name,
+					'html': $scope.email.message
+				}
+			}).success(function(response) {
+				NotificationService.success('Sending Email Done!', 'Email sent!');
+				$scope.email = {
+					name: '',
+					from: '',
+					message: ''
+				}
+			});
+		}
 
 	});
